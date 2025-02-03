a0407dcf5c216635a59c58199c9153a92629c1ff
canton42 <145607162+canton42@users.noreply.github.com>
2025-02-03 12:07:00 -0500

    88 - modify object add a new private field with appropriate accessors
    added new private field refNumber with appropriate accessors.

--- Book.java
+++ Book.java
@@ -12,6 +12,7 @@ class Book
     private String author;
     private String title;
     private int pages;
+    private String refNumber;
     
     /**
      * Set the author and title fields when this object
@@ -19,15 +20,22 @@ class Book
      */
     
     //85 - modify object add a new private field with appropriate accessors
+    //88 - modify object add a new private field with appropriate accessors
     public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        refNumber = "";
 
     }
 
     // Add the methods here ...
+    
+    //88 - modify object add a new private field with appropriate accessors
+    public void setRefNumber(String ref){
+        refNumber = ref;
+    }
     
     //83 - create accessors for String fields
     public String getAuthor(){
@@ -44,6 +52,11 @@ class Book
         return pages;
     }
     
+    //88 - modify object add a new private field with appropriate accessors
+    public String getRefNumber(){
+        return refNumber;
+    }
+    
     //84 - create print utility methods for private fields
     public void printAuthor(){
         System.out.println(author);
@@ -57,9 +70,9 @@ class Book
     //87 - add a printDetails method that displays formatted output for all private fields
     public void printDetails(){
         System.out.println("############");
-        System.out.println("# Title: " + getTitle());
-        System.out.println("# Author: " + getAuthor());
-        System.out.println("# Pages: " + getPages());
+        System.out.println("# Title: " + title);
+        System.out.println("# Author: " + author);
+        System.out.println("# Pages: " + pages);
         System.out.println("############");
     }
 }
